1d13bd1057f22660d75fee15de49231cce7b11f0
Stephanie <stephaniehillier4@gmail.com>
2025-03-04 13:02:48 -0330

    added new custom block for servcice-posts.

---
+++ mindset-blocks/build/service-posts/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '67be2411ef40ad59ac1f');
--- mindset-blocks/mindset-blocks.php
+++ mindset-blocks/mindset-blocks.php
@@ -11,6 +11,11 @@ function mindset_blocks_copyright_date_block_init() {
 	register_block_type( __DIR__ . '/build/copyright-date' );
 	register_block_type( __DIR__ . '/build/company-address' );
 	register_block_type( __DIR__ . '/build/company-email' );
+	
+	register_block_type( __DIR__ . '/src/service-posts', array(
+        'render_callback' => 'fwd_render_service_posts', 
+    ) );
+
 }
 add_action( 'init', 'mindset_blocks_copyright_date_block_init' );
 
@@ -40,5 +45,67 @@ function mindset_register_custom_fields() {
 		)
 	);
 }
+
+function fwd_render_service_posts( $attributes ) {
+    ob_start();
+    ?>
+    <div <?php echo get_block_wrapper_attributes(); ?>>
+
+        <?php
+        // 🔹 First WP_Query - Outputs only post titles with in-page navigation
+        $args_nav = array(
+            'post_type'      => 'service',
+            'posts_per_page' => -1,
+            'post_status'    => 'publish',
+            'orderby'        => 'title',
+            'order'          => 'ASC',
+        );
+
+        $query_nav = new WP_Query( $args_nav );
+
+        if ( $query_nav->have_posts() ) :
+            echo '<nav class="service-nav"><ul>';
+            while ( $query_nav->have_posts() ) : $query_nav->the_post();
+                ?>
+                <li><a href="#post-<?php the_ID(); ?>"><?php the_title(); ?></a></li>
+                <?php
+            endwhile;
+            echo '</ul></nav>';
+            wp_reset_postdata();
+        endif;
+        ?>
+
+        <?php
+        // 🔹 Second WP_Query - Outputs full posts (title + content)
+        $args = array(
+            'post_type'      => 'service',
+            'posts_per_page' => -1,
+            'post_status'    => 'publish',
+            'orderby'        => 'title',
+            'order'          => 'ASC',
+        );
+
+        $query = new WP_Query( $args );
+
+        if ( $query->have_posts() ) :
+            while ( $query->have_posts() ) : $query->the_post();
+                ?>
+                <div id="post-<?php the_ID(); ?>" class="service-post">
+                    <h2><?php the_title(); ?></h2>
+                    <div class="service-content"><?php the_content(); ?></div>
+                </div>
+                <?php
+            endwhile;
+            wp_reset_postdata();
+        else :
+            echo '<p>' . __( 'No service posts found.', 'service-posts' ) . '</p>';
+        endif;
+        ?>
+
+    </div>
+    <?php
+    return ob_get_clean();
+}
+
 add_action( 'init', 'mindset_register_custom_fields' );
 
